0f3955be3b477a5a77b4b0843ef7fe746c7debb7
RStricknine <roys@rdsventures.com>
2026-03-09 20:22:18 -0400

    Implement customer addition to database
    Add SQL connection and customer insertion logic.

--- api/addCustomer/index.js
+++ api/addCustomer/index.js
@@ -1,8 +1,39 @@
+const sql = require("mssql");
+
 module.exports = async function (context, req) {
+
+const config = {
+user: process.env.SQL_USER,
+password: process.env.SQL_PASSWORD,
+server: process.env.SQL_SERVER,
+database: process.env.SQL_DATABASE,
+options: {
+encrypt: true
+}
+};
+
+try {
+
+await sql.connect(config);
+
+await sql.query`
+INSERT INTO CustomersTest (name,email)
+VALUES (${req.body.name},${req.body.email})
+`;
 
 context.res = {
 status: 200,
-body: "API working!"
+body: "Customer saved"
 };
+
+}
+catch(error){
+
+context.res = {
+status: 500,
+body: error.message
+};
+
+}
 
 };
